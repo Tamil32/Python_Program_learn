74264d628618a4456abb5be6dfcff28268d07438
TAMILSELVAN .M <139545703+Tamil32@users.noreply.github.com>
2025-06-25 23:55:01 +0530

    Removed broken submodule and re-added as regular folder

---
+++ New.py
@@ -0,0 +1,12 @@
+print("tamil")
+
+print('123456')
+
+fru_its = ['apple', 'banana', 'cherry']
+print(fru_its[2])
+
+subject = "Math"
+grade = 95
+student = "Priya"
+
+print(student, "got a grade of", grade, "in", subject)
